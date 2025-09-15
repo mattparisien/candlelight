70b3adfe50cb5b5bf523f57084dcdc34eeb0b407
Matt Parisien <63109316+mattparisien@users.noreply.github.com>
2025-09-14 21:03:57 -0400

    s

--- packages/plugins/src/plugins/_lib/config/domMappings.ts
+++ packages/plugins/src/plugins/_lib/config/domMappings.ts
@@ -75,5 +75,5 @@ export const SQSP_BLOCK_SELECTOR_MAP = new Map<string, string>([
 export const SQSP_ENV_SELECTOR_MAP = new Map<string,string>([
   ["DEV", "body.sqs-is-page-editing"],
   ["PROD", "body[data-env='prod']"],
-  ["EDITING", "sqs-is-page-editing"]
+  ["EDITING", "sqs-is-page-editing"] 
 ]);
